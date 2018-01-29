279e9b33140e2c17925a43043dcf9fd5a7cfe9f9
Adam Karpierz <akarpierz@gmail.com>
2018-01-29 20:52:11 +0100

    ...
    ...

--- src/jt/pyjava/_pyjava/_util.py
+++ src/jt/pyjava/_pyjava/_util.py
@@ -6,10 +6,9 @@ from __future__ import absolute_import
 
 from ...jvm.lib.compat import *
 from ...jvm.lib import annotate
-from ...jvm     import jni
 
 
-@annotate(jni.jstring, utf8=bytes)
+@annotate('jt.jvm.JObject', utf8=bytes)
 def from_utf8(utf8):
 
     # Create a Java string from standard UTF-8.
@@ -18,28 +17,27 @@ def from_utf8(utf8):
     # These functions allow to use regular UTF-8, like Python does.
     #
     # @param utf8 Standard UTF-8 representation of the string; might contain null bytes.
-    # @return A reference to a String object, which you might want to clear.
+    # @return A reference (as jvm.JObject) to a String object.
 
     # string = new String(utf8, "UTF-8");
 
     from ctypes import c_char_p
+    from ...jvm import jni
+    from ...jvm.jframe import JFrame
     from ._jvm  import JVM
 
-    jvm  = JVM.jvm
-    jenv = JVM.jenv
+    jt_jvm = JVM.jvm
 
-    size  = len(utf8)
-    jbarr = jenv.NewByteArray(size)
-    jutf8 = jenv.NewStringUTF(b"UTF-8")
-    try:
-        jenv.SetByteArrayRegion(jbarr, 0, size, jni.cast(c_char_p(utf8), jni.POINTER(jni.jbyte)))
-        String_Constructor_bytes = jenv.GetMethodID(jvm._jvm.String.Class,
+    with jt_jvm as (jvm, jenv), JFrame(jenv, 3):
+        jbarr = jenv.NewByteArray(len(utf8))
+        jutf8 = jenv.NewStringUTF(b"UTF-8")
+        jenv.SetByteArrayRegion(jbarr, 0, len(utf8),
+                                jni.cast(c_char_p(utf8), jni.POINTER(jni.jbyte)))
+        String_Constructor_bytes = jenv.GetMethodID(jvm.String.Class,
                                                     b"<init>", b"([BLjava/lang/String;)V")
         jargs = jni.new_array(jni.jvalue, 2)
         jargs[0].l = jbarr
         jargs[1].l = jutf8
-        return jni.cast(jenv.NewObject(jvm._jvm.String.Class,
+        jstr = jni.cast(jenv.NewObject(jvm.String.Class,
                                        String_Constructor_bytes, jargs), jni.jstring)
-    finally:
-        jenv.DeleteLocalRef(jbarr)
-        jenv.DeleteLocalRef(jutf8)
+        return jt_jvm.JObject(jenv, jstr)
